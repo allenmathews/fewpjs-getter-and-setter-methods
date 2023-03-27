3e969c5257a00d90834f6d564d8b9e2560e8eba9
Allen Mathews <allenjoemathews@gmail.com>
2023-03-27 12:59:17 +1100

    commit tree 7aced53516b129fc7025a4ba11ae7ccea20144d6

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return 2 * this.radius
+    }
+
+    get circumference() {
+        return Math.PI * this.diameter
+    }
+
+    get area() {
+        return Math.PI * (this.radius * this.radius)
+    }
+
+    set diameter(newDiameter) {
+        this.radius = newDiameter / 2
+    }
+
+    set circumference(newCircumference) {
+        this.diameter = newCircumference / Math.PI;
+    }
+
+    set area(newArea) {
+        this.radius = Math.sqrt(newArea / Math.PI)
+    }
+}
